76bfae5bb95069e2566616c8fada4399ab0ccb12
andrelisboa1 <201902856@estudantes.ips.pt>
2025-04-25 00:56:18 +0100

    wa

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template, request
-from flask_ngrok import run_with_ngrok, start_ngrok
+import threading
+import time
+import subprocess
 
 app = Flask(__name__)
+has_launched_ngrok = False
 
-run_with_ngrok(app)  # Enable ngrok tunnel
+def launch_ngrok():
+    time.sleep(5)  # Wait for 5 seconds
+    subprocess.Popen(["ngrok", "http", "5000"])  # Launch ngrok in a new process
 
 @app.route("/")
 def home():
+    global has_launched_ngrok
     mode = request.args.get("mode", "light")
+    if not has_launched_ngrok:
+        has_launched_ngrok = True
+        threading.Thread(target=launch_ngrok, daemon=True).start()
     return render_template("home.html", mode=mode)
 
 @app.route("/about")
@@ -16,4 +25,4 @@ def about():
     return render_template("about.html", mode=mode)
 
 if __name__ == "__main__":
-    app.run()
+    app.run()  # Runs as a regular Flask server
